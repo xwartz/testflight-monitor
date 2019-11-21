2981ddedfece3c3a5dff7dc9f879270db633329c
xwartz <stddup@gmail.com>
2019-11-21 16:36:42 +0800

    feat: testflight group mintor

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # testflight
 
 🚑 Manage beta builds of your app, testers, and groups.
+
+## 监控
+
+### 测试人数监控
+
+1. 监控 testflight 某个 group 的测试人数
+2. 当测试超过「限制值」时，自动删除该 group 下的 N 个测试人员
+3. 删除人员顺序按照安装时间
+4. 发送删除信息到 slack
+
+### build 过期监控
+
+1. 监控 testflight 某个 group 的版本期限
+2. 当版本距离过期时间 N 天时，自动发 slack 提示
+
+## reference
+
+- [App Store Connect API](https://developer.apple.com/documentation/appstoreconnectapi)
+- [Unofficial REST API client for Apple's App Store Connect API](https://github.com/aaronsky/appstoreconnect)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,55 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import { v1 } from 'appstoreconnect'
+import { readFileSync } from 'fs'
+import path from 'path'
+import appstore from './config/appstore'
+import config from './config/index'
+
+const privateKey = readFileSync(path.resolve(__dirname, appstore.privateKey))
+const keyId = appstore.keyId
+const issuerId = appstore.issuerId
+
+const groupId = config.groupId
+const maxTesterNum = config.maxTesterNum
+const removeTesterNum = config.removeTesterNum
+
+const freq = 1 * 1000 * 60 // 1min
+
+const token = v1.token(privateKey, issuerId, keyId)
+const api = v1(token)
+
+function start(): void {
+  let timer = null
+  const polling = async () => {
+    try {
+      // get testers info
+      const limit = 200
+      const testers = await v1.testflight.getAllBetaTesterIDsForBetaGroup(api, groupId, { limit })
+      const total = testers.meta.paging.total
+
+      console.log('testers', testers)
+
+      // we need delete some testers
+      if (total >= maxTesterNum) {
+        const maxNum = removeTesterNum > limit ? limit : removeTesterNum
+        const betaTesters = testers.data.slice(0, maxNum)
+        await v1.testflight.removeBetaTestersFromBetaGroup(api, groupId, {
+          data: betaTesters
+        }).catch(err => {
+          // handle error
+          if (err.message !== 'Unexpected end of JSON input') {
+            throw err
+          }
+        })
+        console.log('🌝', `we deleted ${betaTesters.length} guys!`)
+      }
+    } catch (err) {
+      console.log('🌚', err)
+    }
+
+    clearTimeout(timer)
+    timer = setTimeout(polling, freq)
+  }
+  polling()
+}
+
+start()
